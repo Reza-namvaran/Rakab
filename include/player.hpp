1adedb7e24068e693e040a8319e4af34764371b3
Reza-namvaran <reza.namvaran823@gmail.com>
2024-05-25 10:53:02 +0330

    added player class

---
+++ include/player.hpp
@@ -0,0 +1,16 @@
+#ifndef PLAYER_H
+#define PLAYER_H
+
+#include <iostream>
+
+class Player {
+    private:
+        std::string name;
+        unsigned int age;
+        /// TODO: add the vector of cards (deck of cards)
+    
+    public:
+        /// TODO: complete this class
+};
+
+#endif // !PLAYER_H
