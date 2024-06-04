61d80c98209d64df809c466570c9cb0e6fd8a06f
Reza-namvaran <reza.namvaran823@gmail.com>
2024-06-04 19:59:14 +0330

    Fixed cards vector error in player header file

--- include/player.hpp
+++ include/player.hpp
@@ -2,6 +2,7 @@
 #define PLAYER_H
 
 #include <iostream>
+#include <vector>
 
 #include "Card.hpp"
 
@@ -13,7 +14,11 @@ class Player{
         /// TODO: Sign
     public:
         Player(const std::string& p_name, const unsigned int& age);
-
+                
+        /// IMPORTANT: Decide on using full name or first & last name
+        
+        /// NOTE: Added setters, so players can change their personal info if needed. 
+        
         void setPlayerName(const std::string& p_name);
 
         void setPlayerAge(const unsigned int& p_age);
