70c7ed341b94f7eef1410c0db419b1ccab2994c1
Reza-namvaran <reza.namvaran823@gmail.com>
2024-06-11 01:32:39 +0330

    [FEATURE]: Add IO_Interface
    Add an interface class for handling input/output

---
+++ include/IO_Interface.hpp
@@ -0,0 +1,25 @@
+#ifndef IO_INTERFACE_H
+#define IO_INTERFACE_H
+
+#include <iostream>
+
+class IO_Interface{
+  public:
+    IO_Interface();
+
+    ~IO_Interface();
+
+    template <typename T>
+    void print(const T& p_data){
+      std::cout << p_data << "\n";
+    }
+
+    template <typename T>
+    void input(T& p_data){
+      std::cin >> p_data;
+
+      /// IMPORTANT: Decide on using getline()
+    }
+};
+
+#endif // IO_INTERFACE_H
---
+++ src/IO_Interface.cpp
@@ -0,0 +1,5 @@
+#include "IO_Interface.hpp"
+
+IO_Interface::IO_Interface() {}
+
+IO_Interface::~IO_Interface() {}
