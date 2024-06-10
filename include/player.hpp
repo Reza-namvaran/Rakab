045e2090dcd97ab912c8a13d51544e855ab0f7f8
Reza-namvaran <reza.namvaran823@gmail.com>
2024-06-11 01:04:23 +0330

    [REFACTOR]: Add unorderd set for Cards
    Vector of cards has replaced with Unordered set

--- include/player.hpp
+++ include/player.hpp
@@ -2,7 +2,7 @@
 #define PLAYER_H
 
 #include <iostream>
-#include <vector>
+#include <unordered_set>
 
 #include "Card.hpp"
 #include "Sign.hpp"
@@ -12,16 +12,16 @@ class Player{
         std::string name;
         unsigned int age;
         // unsigned int total_score;
-        std::vector<Card*> cards;        
+        std::unordered_set<Card*> hand;        
         Sign sign;
         
     public:
         Player(const std::string& p_name, const unsigned int& age);
-                
+  
         /// IMPORTANT: Decide on using full name or first & last name
         
         /// NOTE: Added setters, so players can change their personal info if needed. 
-        
+
         void setPlayerName(const std::string& p_name);
 
         void setPlayerAge(const unsigned int& p_age);
@@ -30,11 +30,13 @@ class Player{
 
         unsigned int getPlayerAge() const;
 
-        unsigned int getPlayerScore() const;
+        // unsigned int getPlayerScore() const;
 
-        std::vector<Card*> getPlayerCards() const;
+        /// TODO: Do something for hand getter
 
-        Card* selectCard(const Card& p_card);
+        void addCard(const std::unordered_set<Card*>& p_hand);
+
+        Card* selectCard(const std::string& p_card);
 };
 
 #endif
--- src/player.cpp
+++ src/player.cpp
@@ -14,8 +14,13 @@ unsigned int Player::getPlayerAge() const { return this->age; }
 
 // unsigned int Player::getPlayerScore() const { return this->total_score; }
 
-std::vector<Card*> Player::getPlayerCards() const { return this->cards; }
+// std::vector<Card*> Player::getPlayerCards() const { return this->cards; }
 
-Card* Player::selectCard(){
-  
+void Player::addCard(const std::unordered_set<Card*>& p_hand) {
+  this->hand = p_hand;
+  /// NOTE: this is because we don't add single cards 
 }
+
+/* Card* Player::selectCard(const std::string& p_card){
+  /// TODO: Implement this method
+}*/
