8bb51fce5ed11902b89d0ce4df1906094366b442
Reza-namvaran <reza.namvaran823@gmail.com>
2024-06-13 21:33:05 +0330

    [FEATURE]: Add Heroine Class

---
+++ include/Heroine.hpp
@@ -0,0 +1,14 @@
+#ifndef HEROINE_H
+#define HEROINE_H
+
+#include "Special.hpp"
+
+class Heroine : public Special
+{
+public:
+  Heroine(const std::string p_name);
+
+  virtual void use(Player &player, IO_Interface &terminal_handler) override;
+};
+
+#endif // HEROINE_H
---
+++ src/Heroine.cpp
@@ -0,0 +1,7 @@
+#include "Heroine.hpp"
+
+Heroine::Heroine(const std::string p_name) : Special(p_name) {}
+
+void Heroine::use(Player &player, IO_Interface &terminal_handler) {
+  player.setPlayerScore(player.getPlayerScore() + 10);
+}
