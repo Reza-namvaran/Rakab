792e83983ee3a296dbe489e04817f614287fa0f8
Reza-namvaran <reza.namvaran823@gmail.com>
2024-06-13 19:58:42 +0330

    [FEATURE]: Add a method to find round starter

--- include/Match.hpp
+++ include/Match.hpp
@@ -9,12 +9,17 @@
 
 class Match{
   private:
+    IO_Interface terminal_handler;
     std::vector<Player> players;
 
   public:
     Match(std::vector<Player> p_players);
 
-    ~Match(); 
+    ~Match();
+
+    int findRoundStarter(bool first_round);
+
+    void run();
 };
 
 #endif // MATCH_H
--- src/Match.cpp
+++ src/Match.cpp
@@ -3,3 +3,29 @@
 Match::Match(std::vector<Player> p_players) : players(p_players) {}
 
 Match::~Match() {}
+
+int Match::findRoundStarter(bool first_round){
+    if(first_round)
+    {
+        unsigned int min_age = players[0].getPlayerAge();
+        int first_player = 0;
+        for(int idx = 0; idx < players.size(); ++idx)
+        {
+            if(players[idx].getPlayerAge() < min_age)
+            {
+                min_age = players[idx].getPlayerAge();
+                first_player = idx;
+            }
+        }
+
+        this->terminal_handler.print(players[first_player].getPlayerName() + " shall start the game");
+    }
+    else
+    {
+        /// TODO: Implement this section
+    }
+}
+
+void Match::run() {
+    this->findRoundStarter(true);
+}
