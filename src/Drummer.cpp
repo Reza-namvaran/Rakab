47200ca5fe3fcb3175006569ba5bc258ba8eae0b
Mahdi <mahdisadeghi.max@gmail.com>
2024-06-15 00:36:30 +0430

    [REFACTOR]: Improve Drummer class

--- src/Drummer.cpp
+++ src/Drummer.cpp
@@ -2,19 +2,10 @@
 
 Drummer::Drummer(const std::string p_name) : Special("Drummer") {}
 
-void Drummer::use(std::shared_ptr<Player> player, IO_Interface &terminal_handler) {
-    std::vector<std::shared_ptr<Card>> playedCards = player->getCard(false);
-    if (!playedCards.empty()) {
-        terminal_handler.print("Drummer has been played!");
-        terminal_handler.print("All of " + player->getPlayerName() + "'s soldiers' scores will be doubled.\n");
+void Drummer::use(std::shared_ptr<Player> player, IO_Interface &terminal_handler)
+{
 
-        for (const auto &card : playedCards) {
-            if (card->getCardType() == "Soldier") {
-                auto soldier = std::dynamic_pointer_cast<Soldier>(card);
-                if (soldier) {
-                    soldier->setCardScore(soldier->getCardScore() * 2);
-                }
-            }
-        }
-    }
+    terminal_handler.print("Drummer has been played!");
+    terminal_handler.print("All of " + player->getPlayerName() + "'s soldiers' scores will be doubled.\n");
+    player->setPlayerScore(player->getPlayerScore() * 2);
 }
