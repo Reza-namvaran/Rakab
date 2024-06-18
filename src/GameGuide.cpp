d6f7158ba9d6be65b0e94e49cb390ceb5a4657d7
Reza-namvaran <reza.namvaran823@gmail.com>
2024-06-18 16:17:30 +0330

    [FEATURE]: Add GameGuide class
    Implement a class for help commands

---
+++ src/GameGuide.cpp
@@ -0,0 +1,13 @@
+#include "GameGuide.hpp"
+/// TODO: Add to CMakelists
+
+GameGuide::GameGuide() {}
+
+GameGuide::~GameGuide() {}
+
+void GameGuide::getCardInfo(const std::string& card_name){
+  this->terminal_handler.clearScreen();
+  this->terminal_handler.print(this->cards_description[card_name]);
+  this->terminal_handler.print("press any key to continue");
+  this->terminal_handler.onClickInput();
+}
