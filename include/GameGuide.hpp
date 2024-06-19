52ad5fe97ab1a332d9872a002b7b17c740dfc47e
Reza-namvaran <reza.namvaran823@gmail.com>
2024-06-19 10:18:55 +0330

    [REFACTOR]: Modify GameGuide logic
    Move the description to binary files and read from them

--- include/GameGuide.hpp
+++ include/GameGuide.hpp
@@ -3,33 +3,21 @@
 
 #include <unordered_map>
 #include <unordered_set>
+#include <fstream>
+#include <string>
 
 #include "IO_Interface.hpp"
 
 class GameGuide {
   private:
     IO_Interface terminal_handler;
-    std::string game_rules = "This is a prototype";
-    std::unordered_map<std::string, std::string> cards_description = {
-      {"One", "Increases player's score by one"},
-      {"Two", "Increases player's score by two"},
-      {"Three", "Increases player's score by three"},
-      {"Four", "Increases player's score by four"},
-      {"Five", "Increases player's score by five"},
-      {"Six", "Increases player's score by six"},
-      {"Ten", "Increases player's score by ten"},
-      {"Heroine", "For each Heroine in your row, add 10 to your strength.\nThe Heroine is not a soldier,\nso its value is not affected by the Bishop, Winter, the Drummer, or Spring, but its added strength is applied last to avoid any confusion"},
-      {"Spring", "While Spring is in play, check which soldier has the highest value among all player rows.\nAdd 3 to the value of all copies of that card in all player rows.\nNote that the soldier affected by Spring can change during the battle\nOnly winter can chanfe spring"},
-      {"Winter","While Winter is in play, the value of each soldier in all player rows is 1.\nOnly spring can change the winter"},
-      {"Scarecrow", "When you play a Scarecrow, take one of the soldiers from your row back into your hand (if possib0le).\nThe scarecrow cannot be used to take back special cards."},
-      {"Drummer", "While you have a Drummer in your row, double the value of each mercenary in your row.\nYou can play additional Drummers, but they have no effect."},
-    };
+    std::string game_rules;
+    std::unordered_map<std::string, std::string> cards_description;
 
     std::unordered_set<std::string> valid_commands = {
       {"Winter"}, {"Spring"}, {"Heroine"}, {"One"}, {"Two"}, {"Three"}, {"Four"}, {"Five"}, {"Six"}, {"Ten"}, {"Drummer"}, {"Scarecrow"}, {"help"}, {"pass"}, {"help Winter"}, {"help Spring"}
     };
 
-    
   public:
     GameGuide();
 
@@ -39,9 +27,15 @@ class GameGuide {
 
     /// TODO: Implement getGameRules()
 
+    void readGameRules();
+
+    void readCardInfo();
+
     std::unordered_map<std::string, std::string> getDescriptions() const;
 
     void getCardInfo(const std::string& card_name);
+
+    void getGameRules();
 
     std::unordered_set<std::string> getCommands() const;
 };
--- src/GameGuide.cpp
+++ src/GameGuide.cpp
@@ -1,7 +1,10 @@
 #include "GameGuide.hpp"
 /// TODO: Add to CMakelists
 
-GameGuide::GameGuide() {}
+GameGuide::GameGuide() {
+  this->readGameRules();
+  this->readCardInfo();
+}
 
 GameGuide::~GameGuide() {}
 
@@ -13,3 +16,53 @@ void GameGuide::getCardInfo(const std::string& card_name){
 }
 
 std::unordered_map<std::string, std::string> GameGuide::getDescriptions() const { return this->cards_description; }
+
+void GameGuide::getGameRules() {  
+  this->terminal_handler.clearScreen();
+  this->terminal_handler.print(this->game_rules);
+  this->terminal_handler.print("press any key to continue");
+  this->terminal_handler.onClickInput();  
+}
+
+void GameGuide::readGameRules() {
+  std::ifstream input("../data/gamerules.bin", std::ios::binary);
+
+  if(!input.is_open())
+  {
+    this->terminal_handler.print("Error opening the file!");
+    return;
+  }
+  std::string line;
+  
+  while(std::getline(input, line))
+  {
+    this->game_rules += line + "\n";
+  }
+
+  input.close();
+}
+
+void GameGuide::readCardInfo() {
+  std::ifstream input("../data/card_info.bin", std::ios::binary);
+
+  if(!input.is_open())
+  {
+    this->terminal_handler.print("Error opening the file!");
+    return;
+  }
+
+  std::string line;
+
+  while(std::getline(input, line))
+  {
+    auto it = line.find(' ');    
+    if(it != std::string::npos)
+    {
+      std::string key = line.substr(0, it);
+      std::string value = line.substr(it + 1);
+      this->cards_description[key] = value;
+    }
+  }
+
+  input.close();
+}
